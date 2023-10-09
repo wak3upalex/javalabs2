4b60cd80f4cf248be1d62dc4b2cd212a5f7b5c8c
ad230 <aleshadmi@mail.ru>
2023-10-09 16:52:52 +0300

    Necesssary objects created in Main.java

--- lab1/src/lab1/Main.java
+++ lab1/src/lab1/Main.java
@@ -1,7 +1,11 @@
 package lab1;
 
+import java.util.Scanner;
+
 public class  Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in); //scanner object to read input from user
+        Hero hero = new Hero();
+
     }
 }
