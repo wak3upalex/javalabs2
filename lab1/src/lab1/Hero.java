c9b93ebe835b80e5fc9098b82db7fb461dd4b499
ad230 <aleshadmi@mail.ru>
2023-10-11 15:03:25 +0300

    Strategy is setting via Strategy object

--- lab1/src/lab1/Hero.java
+++ lab1/src/lab1/Hero.java
@@ -2,10 +2,9 @@ package lab1;
 
 public class Hero {
     Strategy heroStrategy = new Walk();             //setting default strategy of movemement (walk)
-    private String strategy;
 
-    public void setStrategy(String strategy) {      //setter for strategy
-        this.strategy = strategy;
+    public void setStrategy(Strategy strategy) {    //setter for strategy via Object of Strategy
+        this.heroStrategy = strategy;
     }
 
     public void move(){
