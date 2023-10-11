7a4e7e7f6f06054abd4c7010bc7e2f89e25a832a
ad230 <aleshadmi@mail.ru>
2023-10-11 14:23:37 +0300

    better realisation of Strategy for Hero.java

--- lab1/src/lab1/Hero.java
+++ lab1/src/lab1/Hero.java
@@ -1,7 +1,14 @@
 package lab1;
 
 public class Hero {
-    public void move(){
+    Strategy heroStrategy = new Walk();             //setting default strategy of movemement (walk)
+    private String strategy;
 
+    public void setStrategy(String strategy) {      //setter for strategy
+        this.strategy = strategy;
+    }
+
+    public void move(){
+        heroStrategy.move();
     }
 }
