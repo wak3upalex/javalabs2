8c2fbb04e5682c50e6f615b93e1a29ffc631973e
ad230 <aleshadmi@mail.ru>
2023-10-18 16:06:37 +0300

    Main is implemented

--- lab2/src/lab2/Main.java
+++ lab2/src/lab2/Main.java
@@ -1,7 +1,28 @@
 package lab2;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
 public class Main {
     public static void main(String[] args) {
+        TestMethods test = new TestMethods();                       //obj of Class with methods
+        Method[] methods = TestMethods.class.getDeclaredMethods();  //array for methods
 
+        for (Method method : methods){
+            if (method.isAnnotationPresent(Repeat.class)) {
+                int rep = method.getAnnotation(Repeat.class).value();
+                method.setAccessible(true);
+                int modifier = method.getModifiers();
+                if (Modifier.isPrivate(modifier) || Modifier.isProtected(modifier)){
+                    for (int i = 1; i<=rep; i++){
+                        try {
+                            method.invoke(test, i);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
     }
 }
