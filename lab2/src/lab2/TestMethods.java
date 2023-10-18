d32fe16ed9e55a87b74b5a700517054f6bf2fed7
ad230 <aleshadmi@mail.ru>
2023-10-18 16:03:55 +0300

    Changed type of the message for methods

--- lab2/src/lab2/TestMethods.java
+++ lab2/src/lab2/TestMethods.java
@@ -1,26 +1,26 @@
 package lab2;
 
 public class TestMethods {
-    public void publicMethod1 (String message){
+    public void publicMethod1 (int message){
         System.out.println("Public method 1: " + message);
     }
-    public void publicMethod2 (String message){
+    public void publicMethod2 (int message){
         System.out.println("Public method 2: " + message);
     }
 
     @Repeat(2)
-    protected void protectedMethod1 (String message){
+    protected void protectedMethod1 (int message){
         System.out.println("Protected method 1: " + message);
     }
-    protected void protectedMethod2 (String message){
+    protected void protectedMethod2 (int message){
         System.out.println("Protected method 2: " + message);
     }
 
     @Repeat(4)
-    private void privateMethod1 (String message){
+    private void privateMethod1 (int message){
         System.out.println("Private method 1: " + message);
     }
-    private void privateMethod2 (String message){
+    private void privateMethod2 (int message){
         System.out.println("Private method 2: " + message);
     }
 }
