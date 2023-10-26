98db6a4cb6be3049a8e93879e8330e4536b7c3dc
ad230 <aleshadmi@mail.ru>
2023-10-26 14:13:05 +0300

    Declartion of Collections and method segregte

--- lab3/src/lab3/Main.java
+++ lab3/src/lab3/Main.java
@@ -1,7 +1,30 @@
 package lab3;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
 public class Main {
     public static void main(String[] args) {
-        //TODO
+        Collection <Chordal> srcCollection = Arrays.asList();       //TODO: вписать значения в коллекцию для теста
+
+        //объявление коллекций для ежовых, манулов и рысей
+        Collection <CommonHedgehog> collection1 = new ArrayList<>();
+        Collection <Manul> collection2 = new ArrayList<>();
+        Collection <Lynx> collection3 = new ArrayList<>();
+
+
+        //вызов метода segregate
+
+    }
+
+    //реализация метода segregate
+    public static void segregate(
+            Collection<? extends Chordal> srcCollection,
+            Collection<? super CommonHedgehog> collection1, //collection1 for Commonhedgehog and parents
+            Collection<? super Manul> collection2,   //collection2 for Manuls and parents
+            Collection<? super Lynx> collection3){
+
     }
 }
