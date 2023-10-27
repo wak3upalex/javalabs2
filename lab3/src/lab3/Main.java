80d4f8ff8eac29ef961668c5cfcef6224a545263
ad230 <aleshadmi@mail.ru>
2023-10-27 15:14:04 +0300

    Lab 3 finished. segregate method is tested

--- lab3/src/lab3/Main.java
+++ lab3/src/lab3/Main.java
@@ -1,13 +1,24 @@
 package lab3;
 
-import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 
 public class Main {
     public static void main(String[] args) {
-        Collection <Chordal> srcCollection = Arrays.asList();       //TODO: вписать значения в коллекцию для теста
+        //collections to test programm
+        Collection <Chordal> srcCollection1 = Arrays.asList(new CommonHedgehog(),
+                new Manul(),
+                new Lynx(),
+                new CommonHedgehog(),
+                new Lynx(),
+                new Lynx());
+
+        Collection <Chordal> srcCollection2 = Arrays.asList(new Manul(),
+                new Manul(),
+                new Feline());
+
+        Collection <Chordal> srcCollection3 = Arrays.asList(new Lynx(), new Lynx());
 
         //объявление коллекций для ежовых, манулов и рысей
         Collection <CommonHedgehog> collection1 = new ArrayList<>();
@@ -16,7 +27,10 @@ public class Main {
 
 
         //вызов метода segregate
-
+        segregate(srcCollection1, collection1, collection2, collection3);
+        System.out.println("Обыкновенных ежей: " + collection1.size()); //amount of CommonHedgehogs
+        System.out.println("Манулы: "+ collection2.size());             //amount of Manul
+        System.out.println("Рыси: " + collection3.size());              //amount of Lynx
     }
 
     //реализация метода segregate
