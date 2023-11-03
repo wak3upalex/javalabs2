f604b9313f63b3c24ede963f2a62be31f2f527bc
ad230 <aleshadmi@mail.ru>
2023-11-03 20:23:33 +0300

    method loadDictionary completed

--- lab4/src/lab4/Translator.java
+++ lab4/src/lab4/Translator.java
@@ -1,6 +1,6 @@
 package lab4;
 
-import java.io.File;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -19,5 +19,23 @@ public class Translator {
         if (!(Files.isReadable(path))){
             throw new FileReadException(filename + " access denied");
         } //проверка на доступность файла
+        try(BufferedReader reader = new BufferedReader(new FileReader(filename))){      //читаем файл
+            String line = reader.readLine();    //читаем строчку
+            if (line == null){
+                throw new InvalidFileFormatException("Invalid format in file. Empty line");
+            }
+            while (line != null){
+                String[] keyValue = line.split("//|");
+                if (keyValue.length != 2 || keyValue[0].isBlank() || keyValue[0].isEmpty() || keyValue[1].isBlank()
+                        || keyValue[1].isEmpty()) {      //обработка исключений по строке
+                    throw new InvalidFileFormatException("Invalid format in file. Invalid line");
+                }
+                dictionary.put(keyValue[0].trim().toLowerCase(), keyValue[1].trim());   //добавляем одно слово, как
+                // ключ, второе, как значени
+                line = reader.readLine();
+            }
+        } catch (IOException e){
+            throw new FileReadException("Error in reading file" + e);
+        }
     }
 }
