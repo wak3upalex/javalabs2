ad76068aaf32425885d0dd5192dcc372515399f7
ad230 <aleshadmi@mail.ru>
2023-11-03 19:28:52 +0300

    Translator class is created, dictionary map iss created

---
+++ lab4/src/lab4/Translator.java
@@ -0,0 +1,9 @@
+package lab4;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Translator {
+    Map<String, String> dictionary = new HashMap<>();   //используем Map, чтобы можно было обращаться по ключу
+    
+}
