c5fd02dacf3d97b15aa9369cd882943463280011
ad230 <aleshadmi@mail.ru>
2023-11-07 19:31:19 +0300

    Main is finished. txt files are added

--- lab4/src/lab4/Main.java
+++ lab4/src/lab4/Main.java
@@ -1,7 +1,26 @@
 package lab4;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Translator translator = new Translator();
+        String dictionaryPath = "dictionary.txt";
+        String translatePath = "translate.txt";
+        try {
+            translator.loadDictionary(dictionaryPath);
+            try (BufferedReader reader = new BufferedReader(new FileReader(translatePath))){
+                String line;
+                while ((line = reader.readLine()) != null){
+                    translator.printTranslation(line);
+                }
+            } catch (IOException e){
+                System.err.println("Error while reading file: " + e.getMessage());
+            }
+        } catch (FileReadException | InvalidFileFormatException e) {
+            System.err.println("Error while loading file: " + e.getMessage());
+        }
     }
 }
